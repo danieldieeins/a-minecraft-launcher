c7e606c19e25b61329166b74057c3afea857f702
nerotvlive <daniel.niesmann@outlook.com>
2025-04-06 20:00:47 +0200

    Update application upstream

--- src/main/java/live/nerotv/napp/minecraft/MinecraftModule.java
+++ src/main/java/live/nerotv/napp/minecraft/MinecraftModule.java
@@ -2,6 +2,7 @@ package live.nerotv.napp.minecraft;
 
 import com.google.gson.JsonArray;
 import com.zyneonstudios.nexus.application.Main;
+import com.zyneonstudios.nexus.application.main.NexusApplication;
 import com.zyneonstudios.nexus.application.modules.NexusModule;
 import com.zyneonstudios.nexus.utilities.storage.JsonStorage;
 
@@ -13,51 +14,45 @@ public class MinecraftModule extends NexusModule {
     private static JsonStorage config;
     private final JsonStorage zyndex;
 
-    private final String id = "a-minecraft-module";
-    private final String name = "a Minecraft module";
-    private final String version = "3.0.0-alpha";
-    private final String owner = "nerotvlive";
-    private final String[] contributors = new String[0];
-
     public MinecraftModule() {
-        new File(Main.getApplication().getWorkingPath()+"/modules/a-minecraft-module/").mkdirs();
-        config = new JsonStorage(Main.getApplication().getWorkingPath()+"/modules/a-minecraft-module/config.json");
-        zyndex = new JsonStorage(Main.getApplication().getWorkingPath() + "/modules/a-minecraft-module/java-instances.json");
+        new File(NexusApplication.getInstance().getWorkingPath()+"/modules/a-minecraft-module/").mkdirs();
+        config = new JsonStorage(NexusApplication.getInstance().getWorkingPath()+"/modules/a-minecraft-module/config.json");
+        zyndex = new JsonStorage(NexusApplication.getInstance().getWorkingPath() + "/modules/a-minecraft-module/java-instances.json");
     }
 
     @Override
     public final String getModuleId() {
-        return id;
+        return "a-minecraft-module";
     }
 
     @Override
     public final String getModuleName() {
-        return name;
+        return "a Minecraft module";
     }
 
     @Override
     public final String getModuleVersion() {
-        return version;
+        return "3.0.0-alpha";
     }
 
     @Override
     public final String getModuleOwner() {
-        return owner;
+        return "nerotvlive";
     }
 
     @Override
     public final String[] getModuleContributors() {
-        return contributors;
+        return new String[0];
     }
 
     @Override
     public void onLoad() {
-        Main.getLogger().log("Loading "+getModuleName()+" ("+getModuleId()+") v"+getModuleVersion()+" by "+ Arrays.toString(getModuleAuthors()) +"...");
+
     }
 
     @Override
     public void onEnable() {
-        Main.getLogger().log("Enabling "+getModuleName()+" ("+getModuleId()+") v"+getModuleVersion()+" by "+ Arrays.toString(getModuleAuthors()) +"...");
+        Main.getLogger().log("Enabling "+getModuleName()+" ("+getModuleId()+") v"+getModuleVersion()+" by "+ getModuleOwner() +"...");
         initLibrary();
     }
 
